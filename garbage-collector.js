9c6d7ab9de7f718be687d0c92edd49feed3b8d60
JattMannu <manpreet_firewall@yahoo.com.sg>
2019-11-01 15:47:04 +0800

    Update garbage-collector.js

--- garbage-collector.js
+++ garbage-collector.js
@@ -1,20 +1,20 @@
 // ==UserScript==
 // @name         GC
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  This script is going to clean u2p useless tabs
 // @author       Manpreet Singh
 // @match        http://s3-ap-southeast-1.amazonaws.com/cag-automation-reports/*
 // @match        https://secure.expandrive.com/*
 // @match        https://www.facebook.com/*
-// @grant        none
+// @grant        window.close
 // ==/UserScript==
 
 (function() {
     'use strict';
     setTimeout(function(){
         window.close();
-    }, 60 * 60 * 1000);
+    }, 5 * 60 * 1000);
 })();
 
 
